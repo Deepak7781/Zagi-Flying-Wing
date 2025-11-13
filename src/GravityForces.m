a257092866b0648712be5e9de7424c6c51d39b2e
Deepak7781 <deepak21082005@gmail.com>
2025-11-13 06:45:26 +0530

    add: Eqm.m

---
+++ src/GravityForces.m
@@ -0,0 +1,14 @@
+function Gravity = GravityForce(params, phi, theta, psi)
+
+R_psi = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+R_theta = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+R_phi = [1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi)];
+
+
+R = R_phi*R_theta*R_psi;
+
+fg = params.mass*params.g;
+
+Gravity = R * [0; 0; fg];
+
+end
